9c048d0223aa439d0b170065be8f26cad0531268
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-11-02 07:44:42 +0530

    setup server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("server running at 3000");
+import express from "express";
+
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcombe to subscription api.");
+});
+
+app.listen(port, () => {
+  console.log(`server is running at : http://localhost:${port}`);
+});
+
+export default app;
