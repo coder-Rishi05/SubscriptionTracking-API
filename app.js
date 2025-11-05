607136abbc824d2015cb746747110ef585e754df
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-11-05 20:45:14 +0530

    completed signUp function and fix error

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const port = PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
-app.use(cookieParser)
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter); // it means : /api/v1/auth/sign-up
 app.use("/api/v1/users", userRoute); // it means : /api/v1/auth/sign-up
