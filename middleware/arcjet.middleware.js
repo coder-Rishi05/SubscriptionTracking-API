fa1bafe3c693e478190649a8ce6c2aa3529806ce
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-11-08 15:02:01 +0530

    to be fix : arcjet bot problem

--- middleware/arcjet.middleware.js
+++ middleware/arcjet.middleware.js
@@ -1,16 +1,19 @@
-import aj from "../config/aecjet.js";
+import aj from "../config/arcjet.js";
 
 const arcMiddleware = async (req, res, next) => {
   try {
     const decision = await aj.protect(req, { requested: 1 });
 
     if (decision.isDenied()) {
-      if (decision.reason.isRateLimit()) return res.status(429).json({ error: "Rate limit exceed" });
-    //   if (decision.reason.isBot())  return res.status(403).json({ error: "Bot detected" });
+      if (decision.reason.isRateLimit()) {
+        return res.status(429).json({ error: "Rate limit exceed" });
+      }
+      if (decision.reason.isBot()) {
+        return res.status(403).json({ error: "Bot detected" });
+      }
 
       return res.status(403).json({ error: "Access denied" });
     }
-
     next();
   } catch (error) {
     console.log(`Arcjet Middleware Error ${error} `);
